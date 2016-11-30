e927200ad5b2be05e67f9c3c7e2be6f21b47794d
Suguru Tokuda <suguru.tokuda@gmail.com>
2016-11-30 16:43:13 -0600

    userIndex updated

--- src/java/email/Email.java
+++ src/java/email/Email.java
@@ -8,6 +8,8 @@ package email;
 
 
 import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
 import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -82,33 +84,26 @@ public class Email {
     
     public boolean confirmationEmail(Profile profile) {
         boolean sent = false;
-        // Recipient's email ID needs to be mentioned.
+        
+          // Recipient's email ID needs to be mentioned.
         String to = profile.getEmail();
 
         // Sender's email ID needs to be mentioned
         String from = "stokuda@ilstu.edu";
 
         // Assuming you are sending email from this host
-        String host = "m.outlook.com";
+        String host = "smtp.ilstu.edu";
 
         // Get system properties
-        Properties props = System.getProperties();
+        Properties properties = System.getProperties();
 
         // Setup mail server
-        props = new Properties();
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.port", "587"); // if needed
-        props.put("mail.smtp.host", "m.outlook.com"); // if needed
-        props.put("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.host", host);
+//        properties.setProperty("mail.user", "yourID"); // if needed
+//        properties.setProperty("mail.password", "yourPassword"); // if needed
+
         // Get the default Session object.
-        Session session = Session.getDefaultInstance(props);
-        session = Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("stokuda@ilstu.edu",
-                        "Thedarkkight121");
-            }
-        });
+        Session session = Session.getDefaultInstance(properties);
 
         try {
             // Create a default MimeMessage object.
@@ -118,11 +113,12 @@ public class Email {
             message.setFrom(new InternetAddress(from));
 
             // Set To: header field of the header.
-            message.setRecipient(Message.RecipientType.TO,
+            message.addRecipient(Message.RecipientType.TO,
                     new InternetAddress(to));
 
             // Set Subject: header field
-            message.setSubject("Your Account has been created");
+            message.setSubject("Welcome to Submissions Portal");
+
             // Send the actual HTML message, as big as you like
             message.setContent("Hi " + profile.getFirstName() + "," + "<br/>" + "You have been Succesfully SignedUp." + "<br/>"
                     + "Your UserID :" + profile.getUserID() + "<br/>"
@@ -132,13 +128,72 @@ public class Email {
 
             // Send message
             Transport.send(message);
-            sent = true;
             System.out.println("Sent message successfully....");
-
         } catch (MessagingException mex) {
             mex.printStackTrace();
-            sent = false;
         }
+        
+        
+        
+        
+//        boolean sent = false;
+//        // Recipient's email ID needs to be mentioned.
+//        String to = profile.getEmail();
+//
+//        // Sender's email ID needs to be mentioned
+//        String from = "stokuda@ilstu.edu";
+//
+//        // Assuming you are sending email from this host
+//        String host = "m.outlook.com";
+//
+//        // Get system properties
+//        Properties props = System.getProperties();
+//
+//        // Setup mail server
+//        props = new Properties();
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.smtp.port", "587"); // if needed
+//        props.put("mail.smtp.host", "m.outlook.com"); // if needed
+//        props.put("mail.smtp.auth", "true");
+//        // Get the default Session object.
+//        Session session = Session.getDefaultInstance(props);
+//        session = Session.getInstance(props, new Authenticator() {
+//            @Override
+//            protected PasswordAuthentication getPasswordAuthentication() {
+//                return new PasswordAuthentication("stokuda@ilstu.edu",
+//                        "Thedarkkight121");
+//            }
+//        });
+//
+//        try {
+//            // Create a default MimeMessage object.
+//            MimeMessage message = new MimeMessage(session);
+//
+//            // Set From: header field of the header.
+//            message.setFrom(new InternetAddress(from));
+//
+//            // Set To: header field of the header.
+//            message.setRecipient(Message.RecipientType.TO,
+//                    new InternetAddress(to));
+//
+//            // Set Subject: header field
+//            message.setSubject("Your Account has been created");
+//            // Send the actual HTML message, as big as you like
+//            message.setContent("Hi " + profile.getFirstName() + "," + "<br/>" + "You have been Succesfully SignedUp." + "<br/>"
+//                    + "Your UserID :" + profile.getUserID() + "<br/>"
+//                    + "<br/>" + "Please keep in touch." + "<br/>" + "Regards," + "<br/>" + "Team Project353"
+//                    + "<br/>" + "<img src=\"http://content.sportslogos.net/logos/32/707/thumbs/wgpjcd57fikjji1qy97f2gsqk.gif\">",
+//                    "text/html");
+//
+//            // Send message
+//            Transport.send(message);
+//            sent = true;
+//            System.out.println("Sent message successfully....");
+//
+//        } catch (MessagingException mex) {
+//            mex.printStackTrace();
+//            sent = false;
+//        }
         return sent;
     }
     
