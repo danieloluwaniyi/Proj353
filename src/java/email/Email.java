a97e5e883568d2147fcebcb3aed398a8c64af6a7
vsneh <vyas.sneh@yahoo.com>
2016-12-02 19:55:47 -0600

    admin controlls and features

--- src/java/email/Email.java
+++ src/java/email/Email.java
@@ -122,65 +122,65 @@ public class Email implements Serializable {
     }
         
         
-    public boolean confirmationEmail(Profile profile) {
-        boolean sent = false;
-        // Recipient's email ID needs to be mentioned.
-        String to = profile.getEmail();
-
-        // Sender's email ID needs to be mentioned
-        String from = "stokuda@ilstu.edu";
-
-        // Assuming you are sending email from this host
-        String host = "m.outlook.com";
-
-        // Get system properties
-        Properties props = System.getProperties();
-
-        // Setup mail server
-        props = new Properties();
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.port", "587"); // if needed
-        props.put("mail.smtp.host", "m.outlook.com"); // if needed
-        props.put("mail.smtp.auth", "true");
-        // Get the default Session object.
-        Session session = Session.getDefaultInstance(props);
-        session = Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("svyas12@ilstu.edu",
-                        "Sohanlal1!");
-            }
-        });
-
-        try {
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
-
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
-            message.setRecipient(Message.RecipientType.TO,
-                    new InternetAddress(to));
-
-            // Set Subject: header field
-            message.setSubject("Your Account has been created");
-            // Send the actual HTML message, as big as you like
-            message.setContent("Hi " + SignUpetails.getFirstName() + "," + "<br/>" + "You have won the contest based on user ratings." + "<br/>",
-                    "text/html");
-
-            // Send message
-            Transport.send(message);
-            sent = true;
-            System.out.println("Message sent successfully....");
-
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
-            sent = false;
-        }
-        return sent;
-
-    }
+//    public boolean confirmationEmail(Profile profile) {
+//        boolean sent = false;
+//        // Recipient's email ID needs to be mentioned.
+//        String to = profile.getEmail();
+//
+//        // Sender's email ID needs to be mentioned
+//        String from = "stokuda@ilstu.edu";
+//
+//        // Assuming you are sending email from this host
+//        String host = "m.outlook.com";
+//
+//        // Get system properties
+//        Properties props = System.getProperties();
+//
+//        // Setup mail server
+//        props = new Properties();
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.smtp.port", "587"); // if needed
+//        props.put("mail.smtp.host", "m.outlook.com"); // if needed
+//        props.put("mail.smtp.auth", "true");
+//        // Get the default Session object.
+//        Session session = Session.getDefaultInstance(props);
+//        session = Session.getInstance(props, new Authenticator() {
+//            @Override
+//            protected PasswordAuthentication getPasswordAuthentication() {
+//                return new PasswordAuthentication("svyas12@ilstu.edu",
+//                        "Sohanlal1!");
+//            }
+//        });
+//
+//        try {
+//            // Create a default MimeMessage object.
+//            MimeMessage message = new MimeMessage(session);
+//
+//            // Set From: header field of the header.
+//            message.setFrom(new InternetAddress(from));
+//
+//            // Set To: header field of the header.
+//            message.setRecipient(Message.RecipientType.TO,
+//                    new InternetAddress(to));
+//
+//            // Set Subject: header field
+//            message.setSubject("Your Account has been created");
+//            // Send the actual HTML message, as big as you like
+//            message.setContent("Hi " + SignUpetails.getFirstName() + "," + "<br/>" + "You have won the contest based on user ratings." + "<br/>",
+//                    "text/html");
+//
+//            // Send message
+//            Transport.send(message);
+//            sent = true;
+//            System.out.println("Message sent successfully....");
+//
+//        } catch (MessagingException mex) {
+//            mex.printStackTrace();
+//            sent = false;
+//        }
+//        return sent;
+//
+//    }
 
     public boolean confirmationEmail(Profile profile) {
 
